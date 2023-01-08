30b469aa5534a66ed730118242fbc1189f6a9bf9
Ayushsingh05 <singhayush28321@gmail.com>
2023-01-08 22:56:49 +0530

    post module created

--- models/user.js
+++ models/user.js
@@ -1,10 +1,47 @@
 const mongoose= require('mongoose');
-
+const {ObjectId} = mongoose.Schema.Types;
 const userSchema = new mongoose.Schema({
-name:{type:String,required:true},
-email:{type:String,required:true},
-password:{type:String,required:true}
-// post:{type:Array}
+
+          name:
+           {
+            type: String,
+             required:true,
+              trim:true,
+             minLength:2,
+             maxLength:30
+             },
+             email:
+            {
+            type: String,
+            required:true,
+            trim:true,
+            unique:true
+            },
+            password:
+            {
+            type: String,
+            required:true,
+            trim:true,
+            minLength:2,
+            },
+            profile_pic: {
+                type: "String",
+                required: true,
+                default:"https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+              },
+          followers:[{
+                       type:ObjectId,
+                       ref:"UserModel"
+                    }],
+          following:[{
+                       type:ObjectId,
+                       ref:"UserModel"
+                     }],
+          date:
+          {
+            type:Date,
+            default:Date.now
+          },
 })
 
 const UserModel= mongoose.model("user",userSchema);
