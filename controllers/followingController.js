7b803adbc4cb9d50ca0fb7acc4f1b9cd5f9927b1
Ayushsingh05 <singhayush28321@gmail.com>
2023-01-11 22:44:48 +0530

    following done

---
+++ controllers/followingController.js
@@ -0,0 +1,19 @@
+const userModel = require('../models/user');
+
+const followingListController = async (data) =>{
+                                          try
+                                          {
+                                            const {email} = data;
+
+                                              const followingListDetails = await userModel.findOne({email}).populate("following");
+                                    
+                                              return followingListDetails
+
+                                          }catch(e)
+                                            {
+                                                console.error(e);
+                                                return {"error" : e.message}
+                                            }
+                                       }
+
+module.exports = followingListController;
