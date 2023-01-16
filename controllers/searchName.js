c0f333b6b3c6af8ef37f7ae90d221ec8f77304bf
Ayushsingh05 <singhayush28321@gmail.com>
2023-01-16 15:51:05 +0530

    user search updated

---
+++ controllers/searchName.js
@@ -0,0 +1,26 @@
+// const postModel = require("../models/post");
+const userModel = require("../models/userModel");
+
+const searchProfileController = async (data) => {
+    // data=req.body
+  try {
+    const { name } = data;
+
+    if (!name) {
+      return  ({ error: "Please add valid profile name to search" });
+    }
+
+    const user_exits = await userModel.findOne({ name });
+
+    if (user_exits) {
+      return (user_exits);
+    } else {
+      return ({ error: "Profile not found" });
+    }
+  } catch (e) {
+    console.log(e);
+    return ({ error: "Profile not found" });
+  }
+};
+
+module.exports = searchProfileController;
