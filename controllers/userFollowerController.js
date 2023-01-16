b70677397bf3fe790486c2aafb0f158d0c643aaf
Ayushsingh05 <singhayush28321@gmail.com>
2023-01-16 15:38:31 +0530

    user Follower updated

--- controllers/userFollowerController.js
+++ controllers/userFollowerController.js
@@ -1,33 +1,36 @@
-const postModel = require('../models/postModel');
-const signupModel = require("../models/signupModel");
+const postModel = require('../models/post');
+const userModel = require("../models/user");
 
-const profileFollowersController = async (req, res) =>{
-                                              try
-                                              {
-                                                console.log(req.body.followId);
-                                                signupModel.findByIdAndUpdate(req.body.followId,
-                                                                              { $push:{followers:req.user._id} },
-                                                                              { new:true },
-                                                                              (err,result)=>{
-                                                                                              if(err)
-                                                                                              {
-                                                                                                return res.status(422).json({error:err});
-                                                                                              }
-                                                signupModel.findByIdAndUpdate(req.user._id,
-                                                                              {$push:{following:req.body.followId}},
-                                                                              {new:true}).select("-password")
-                                                                              .then(result =>{
-                                                                                        res.status(200).json(result);
-                                                                                             }
-                                                                                    )
-                                                                                            }
-                                                                              )
 
-                                              }catch (e)
-                                                {
-                                                  console.log(e);
-                                                  return res.status(404).json({error:"Profile followers not found"});
-                                                }
-                                          }
+const userFollowersController = async (data,user) =>{
+        try
+        {
+        //  console.log(req.body.followId);
+        // data =reqy.body;
+        // user=req.user
+         userModel.findByIdAndUpdate(data.followId,
+          { $push:{followers:user._id} },
+          { new:true },
+          (err,result)=>{
+          if(err)
+          {
+          return ({error:err});
+          }
+          userModel.findByIdAndUpdate(user._id,
+          {$push:{following:data.followId}},
+          {new:true}).select("-password")
+           .then(result =>{
+          return (result);
+          }
+          )
+          }
+          )
 
-module.exports = profileFollowersController;
+          }catch (e)
+          {
+          console.log(e);
+          return {error:"Profile followers not found"};
+          }
+           }
+
+module.exports = userFollowersController;
